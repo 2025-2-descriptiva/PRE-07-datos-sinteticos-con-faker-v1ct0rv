5aa106249476b901cc3d5bef13b77155dc5deeff
Victor Velásquez <vivelasquezc@unal.edu.co>
2025-09-26 18:01:07 -0500

    PRE-07

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,10 +1,12 @@
 """Autograding script"""
 
 import os
+import homework.generate_data as gd
 
 
 def test_01():
     """Test app"""
 
+    gd.run(n_drivers=100, n_timesheet=1000)
     assert os.path.exists("files/drivers.csv")
     assert os.path.exists("files/timesheet.csv")
